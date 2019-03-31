4944b97f18b143833d0e25bd020e635be6b2f1a1
siwakorn21 <49062166+siwakorn21@users.noreply.github.com>
2019-04-01 02:25:34 +0700

    Update Class of cards.py

--- Class of cards.py
+++ Class of cards.py
@@ -1,3 +1,30 @@
+# Card-sorted
+#sort the cards 
+#input
+#5
+#A spade
+#K heart
+#K club
+#7 diamond
+#2 spade
+#---------------
+#output
+#1
+#10
+#10
+#7
+#2
+#-----------
+#1
+#0
+#7
+#9
+#------------
+#(7 diamond)
+#(K club)
+#(K heart)
+#(A spade)
+#(2 spade)
 class Card :
     def __init__(self,value,suit):
         self.value = value
